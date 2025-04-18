70cbe499a6b264db4d40f62c6c76eeea9019655d
thejokers69 <thejokkers69@gmail.com>
2025-04-18 18:44:05 +0100

    feat: Add homepage and deployment scripts to package.json; update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/quiz-name/',
   plugins: [react()],
   server: {
     historyApiFallback: true
